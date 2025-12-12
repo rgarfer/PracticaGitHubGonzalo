aec0d07822fe39499243371d18b597b94db8fedf
RaulGarFer <raulgarciaferna@gmail.com>
2025-12-12 12:20:02 +0100

    actualizacion en el ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,19 +2,25 @@ package tema5.tarea51;
 
 public class Ejercicio1 {
 
-	public static void main(String[] args) {
-		
-		byte[] valor = new byte[5];
-		
-		valor[0] = 0;
-		valor[1] = 1;
-		valor[2] = 2;
-		valor[3] = 3;
-		valor[4] = 4;
+    public static void main(String[] args) {
+        
+        byte[] valor = new byte[5];
+        
+        valor[0] = 0;
+        valor[1] = 1;
+        valor[2] = 2;
+        valor[3] = 3;
+        valor[4] = 4;
 
-		for (int i = 0; i < valor.length; i++) {
-			valor[i] = (byte) (Math.random() * 259 - 128);
-		}
-	}
+        // Rellenar con valores aleatorios entre -128 y 127
+        for (int i = 0; i < valor.length; i++) {
+            valor[i] = (byte) (Math.random() * 256 - 128);
+        }
 
+        // Nueva funcionalidad: mostrar los valores generados
+        System.out.println("Valores aleatorios generados:");
+        for (byte b : valor) {
+            System.out.println(b);
+        }
+    }
 }
