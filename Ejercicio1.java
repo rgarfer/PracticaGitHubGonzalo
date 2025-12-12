caf0ccfc8e273fd0284611dc7eabb480a3f65880
Gonzaloflores345 <floresgonzalofg@gmail.com>
2025-12-12 12:01:15 +0100

    Subo ejercicio programación

---
+++ Ejercicio1.java
@@ -0,0 +1,20 @@
+package tema5.tarea51;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		byte[] valor = new byte[5];
+		
+		valor[0] = 0;
+		valor[1] = 1;
+		valor[2] = 2;
+		valor[3] = 3;
+		valor[4] = 4;
+
+		for (int i = 0; i < valor.length; i++) {
+			valor[i] = (byte) (Math.random() * 259 - 128);
+		}
+	}
+
+}
